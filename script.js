488a155d77cafa1f249c3eb0ecde64dfcf4b672d
davidsvaldo <david.henrique.271299@gmail.com>
2023-04-18 20:18:58 -0300

    primeiro comit

---
+++ script.js
@@ -0,0 +1,14 @@
+const btn = document.getElementById("redefinir");
+
+btn.addEventListener("click", () => {
+    const cpf = document.getElementById("cpf").value;
+    const nascimento = document.getElementById("nascimento").value;
+    const senha = document.getElementById("senha").value;
+    const senhaConf = document.getElementById("senhaConf").value;
+
+    // Faz o tratamento dos dados, como validar o CPF e verificar se as senhas digitadas são iguais
+    // ...
+
+    // Exibe uma mensagem de sucesso ou erro, dependendo dos resultados da validação
+    // ...
+});
